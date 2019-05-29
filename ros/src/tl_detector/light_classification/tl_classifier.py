9fa221e851f3f4caa90d508be494ce072acd5b7e
root <soya0804@gmail.com>
2019-05-29 13:25:50 +0000

    recommit

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -2,6 +2,11 @@
 
 from styx_msgs.msg import TrafficLight
 from yolo import YOLO
+from keras.models import load_model
+import numpy as np
+import os
+import cv2
+PATH = os.getcwd()
 
 class TLClassifier(object):
     def __init__(self):
@@ -10,9 +15,8 @@ class TLClassifier(object):
         self.detector = YOLO()
         
         # Light state classifier - > pretrained CNN
-        #self.classifier = model.load()
-        
-        pass
+        self.classifier = load_model(PATH + '/light_classification/tl_classifier.h5',compile=False)
+        print('Classifier was loaded correctly.')
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
@@ -26,10 +30,15 @@ class TLClassifier(object):
         """
         #TODO implement light color prediction
         
-        # (1) Get ROI 
+        # (1) Get ROI (Detect traffic lights)
         image, tl_imgs = self.detector.detect_image(image)
-        for i in range(0,len(tl_imgs)-1):
-            print(i)
-            # (2) Input CNN
+        if (tl_imgs):
+            # For each tdetected lights
+            for i in range(0,len(tl_imgs)-1):
+                # (2) Input CNN
+                img = np.asarray(tl_imgs[i])
+                img = cv2.resize(img, (32,32))
+                prd = np.argmax(self.classifier.predict(img.reshape([1,32,32,3])))
+                print(prd)
         
         return TrafficLight.GREEN
